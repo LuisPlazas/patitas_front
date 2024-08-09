309ba321c6057ad0fecf27dfbd7de9b2ef156d1b
AndresMosquera9005 <andrubjj@gmail.com>
2024-08-09 10:02:11 -0500

    11 cambios, 3 fotos, nuevo pomponente card, se modificaron

--- src/app/component/adopcion/adopcion.component.ts
+++ src/app/component/adopcion/adopcion.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../card/card.component";
 
 @Component({
   selector: 'app-adopcion',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './adopcion.component.html',
-  styleUrl: './adopcion.component.css'
+  styleUrls: ['./adopcion.component.css']
 })
 export class AdopcionComponent {
 
